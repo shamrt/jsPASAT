e743f15a7c2fa1dedf5356d2b24c94421d45090b
Shane Martin <dev.sh@nemart.in>
2015-08-26 12:28:25 -0400

    Switch to multi-stim-multi-response plugin

--- public/js/practice.js
+++ public/js/practice.js
@@ -1,5 +1,6 @@
 // NOTE: Prompt sound used in this file is courtesy SoundBible.com - http://soundbible.com/1252-Bleep.html
 
+var beep = new Howl({urls: ['../audio/bleep.mp3', '../audio/bleep.ogg']});
 var practice = [];
 
 // // pre-block notice
@@ -10,30 +11,55 @@ var practice = [];
 // }
 // practice.push(pre_practice_block_1);
 
-// practice block 1
-function beep_stimuli(num) {
-  var num = (typeof num !== "undefined" && typeof num == "number" ) ? num : 10;
-  var beep_stimulus = "../audio/bleep.mp3";
-  var beeps = [];
-  for (var i = 0; i < num; i++) {
-    beeps.push(beep_stimulus);
+// keep track of status
+var current_trial_val,
+    last_trial_val;
+
+
+function getExpectedValue(trial_data) {
+  var correct = false,
+      response
+}
+
+
+function createStimuli(trials) {
+  var stimuli = [];
+  for (var i = 0; i < trials.length; i++) {
+    var trial_stimuli = [];
+    for (var j = 0; j < trials[i].length; j++) {
+      trial_html = "<h1>" + trials[i][j] + "</h1><script>beep.play()</script>"
+      trial_stimuli.push(trial_html);
+    }
+    stimuli.push(trial_stimuli);
   }
-  return beeps;
+  return stimuli;
 }
 
-function visual_prompt() {
-  return "<h1>5</h1>"
+
+function createChoicesArray(stimuli) {
+  choices = [];
+  for (var i = 0; i < stimuli.length; i++) {
+    choices.push(_.range(48,58));
+  }
+  return choices;
 }
 
+
+// practice block 1
 var practice_block_1 = {
-  type: "single-audio",
-  stimuli: beep_stimuli(2),
-  prompt: visual_prompt(),
+  type: "multi-stim-multi-response",
+  stimuli: createStimuli([[9], [1], [3], [5], [2], [6]]),
+  is_html: true,
   timing_response: 1000,
   timing_post_trial: 3000,
   response_ends_trial: false,
-  data: { expected: 5 }
+  data: { expected: 5 },
+  on_finish: function() {
+    console.log(jsPsych.data.getLastTrialData())
+  }
 }
+practice_block_1['choices'] = createChoicesArray(practice_block_1['stimuli']);
+console.log(practice_block_1)
 practice.push(practice_block_1);
 
 jsPsych.init({

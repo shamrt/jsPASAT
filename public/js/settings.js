cef51a62cdd916ccf6adca89ee45bc3af2dc1e8a
Shane Martin <dev.sh@nemart.in>
2015-09-23 17:49:40 -0400

    Update blocks per condition setting

--- public/js/settings.js
+++ public/js/settings.js
@@ -7,7 +7,7 @@ var jsPASAT = {
   // Condition
   // --------------------------------
   // Sets the number of blocks in
-  'BLOCKS_PER_CONDITION': [6, 7, 8, 9],
+  'BLOCKS_PER_CONDITION': [5, 6, 7, 8, 9],
 
   // Pace of PASAT trial presentation
   // --------------------------------
